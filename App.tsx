14a4d8d0c4d562e9293450f369e542b8f4cb153e
JOAO PAULO RAMOS LOPES <joao.prlopes2@senacsp.edu.br>
2025-09-22 22:34:28 -0300

    primeiro commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,82 @@
+import React from 'react'; 
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Text, View, TextInput } from 'react-native';
+
+
+interface MeuBotaoProps { 
+  descricao: string
+}
+
+const MeuBotao : React.FC<MeuBotaoProps> = ( props ) => { 
+  return ( 
+    <View style={{backgroundColor: "cornflowerblue",
+          paddingHorizontal: 20, paddingVertical: 15,
+          borderRadius: 15
+        }}>
+      <Text style={{color: "white", fontWeight: "bold",
+        fontSize: 18
+      }}>{props.descricao}</Text>
+    </View>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+    <View style={{flex: 1,
+      flexDirection: "column",
+      justifyContent: "space-evenly",
+      padding: 20,
+      backgroundColor: "yellow",
+      borderRadius: 30}}> 
+      <Text style={{
+          flex: 1,
+          fontSize: 32,
+          fontWeight: "bold",
+          fontFamily: "arial",
+          backgroundColor: "lightcyan",
+          textAlign: "center"
+        }}>Agenda</Text>
+      <View style={{flexDirection: "row",
+        backgroundColor: "lightyellow",
+        justifyContent: "space-between",
+        flex: 1
+      }}>
+        <Text>Nome do contato:</Text>
+        <TextInput style={{
+          left: 100,
+          top: 100,  backgroundColor: "lightcyan", 
+          borderColor: "red", borderWidth: 2,
+          borderRadius: 30}} />
+      </View>
+      <View style={{flexDirection: "row",
+        backgroundColor: "lightyellow",
+        justifyContent: "space-between",
+        flex: 1
+      }}>
+        <Text>Nome do contato:</Text>
+        <TextInput style={{
+          left: 100,
+          top: 100,  backgroundColor: "lightcyan", 
+          borderColor: "red", borderWidth: 2,
+          borderRadius: 30}} />
+      </View>
+      <View style={{flexDirection: "row",
+        backgroundColor: "lightyellow",
+        justifyContent: "space-between",
+        flex: 1
+      }}>
+        <Text>Nome do contato:</Text>
+        <TextInput style={{
+          left: 100,
+          top: 100,  backgroundColor: "lightcyan", 
+          borderColor: "red", borderWidth: 2,
+          borderRadius: 30}} />
+      </View>
+      <View style={{flex: 0.3, flexDirection: "row"}}>
+        <MeuBotao descricao="Salvar"/>          
+        <MeuBotao descricao="Pesquisar"/>                  
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
